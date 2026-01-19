54e8fccda2958a2dbe71618276e280c80ed67844
Abhishek Aggarwal <abhi@Mac.hsd1.wa.comcast.net>
2026-01-19 11:36:59 -0800

    Rename to day coach app

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Habits Logger',
+      title: 'Day Coach',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2E7D32)),
         useMaterial3: true,
